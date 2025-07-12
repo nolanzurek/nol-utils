96ce337adf01c33146481010cc133659c610cb2e
Nolan Zurek <nolanzurek@gmail.com>
2025-07-11 23:35:43 -0700

    implement obsidian vault init

--- obsidian/init_obsidian_vault.sh
+++ obsidian/init_obsidian_vault.sh
@@ -13,7 +13,30 @@ fi
 
 new_vault_path="${OBSIDIAN_VAULTS_DIR}/${new_vault_name}"
 
-mkdir -p "$new_vault_path/.obsidian"
+if [ -d "$new_vault_path" ]; then
+    echo "Error: Vault '$new_vault_name' already exists in $OBSIDIAN_VAULTS_DIR"
+    exit 1
+fi
+
+# create a private github repo for the new vault, then clone it
+
+gh repo create "$new_vault_name" --private
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to create GitHub repository '$new_vault_name'."
+    echo "Ensure the GitHub CLI is installed and authenticated using 'gh auth login'."
+    exit 1
+fi
+
+cd "$OBSIDIAN_VAULTS_DIR"
+git clone "https://github.com/$GITHUB_USERNAME/$new_vault_name.git"
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to clone the GitHub repository '$new_vault_name'."
+    exit 1
+fi
+
+cd "$new_vault_name"
+
+# mkdir -p "$new_vault_path/.obsidian"
 
 if [ ! -d "$OBSIDIAN_CANONICAL_VAULT_PATH" ]; then
     echo "Error: Canonical vault not specified, using any vault in $OBSIDIAN_VAULTS_DIR"
@@ -21,17 +44,16 @@ if [ ! -d "$OBSIDIAN_CANONICAL_VAULT_PATH" ]; then
     echo "Using canonical vault: $canonical_vault"
 else
     canonical_vault="$OBSIDIAN_CANONICAL_VAULT_PATH"
+    echo "Using canonical vault: $canonical_vault"
 fi
 
-cp -r "$canonical_vault/.obsidian" "$new_vault_path/"
+cp -r "$canonical_vault/.obsidian" .
 
-# init git repo
-cd "$new_vault_path"
-git init
 git add .
-git commit -m "Initialize Obsidian vault with default settings"
-git push -u origin main
+git commit -m "Initial commit for new Obsidian vault: $new_vault_name"
+git push origin main
 
+echo "New Obsidian vault '$new_vault_name' created successfully at $new_vault_path."
 cd "$working_dir"
 
 exit 0
