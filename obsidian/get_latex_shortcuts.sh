9c8cf05cf067267242a85092fe62c331556c0d60
Nolan Zurek <nolanzurek@gmail.com>
2025-07-11 19:19:59 -0700

    switch grep to egrep

--- obsidian/get_latex_shortcuts.sh
+++ obsidian/get_latex_shortcuts.sh
@@ -6,6 +6,6 @@ outputs_dir=$2
 cat $(find $working_dir -type f -path "*/.obsidian/plugins/quick-latex/data.json" -maxdepth 5) \
     | jq -r ".customShorthand_parameter" \
     | sed 's/;*$//;s/$/;/' \
-    | grep -v '^;$' \
+    | egrep -v '^;$' \
     | sort | uniq \
     > "$outputs_dir/obsidian_latex_shortcuts.txt"
--- obsidian/get_plugins.sh
+++ obsidian/get_plugins.sh
@@ -3,6 +3,6 @@
 vaults_dir=$1
 outputs_dir=$2
 
-ls $(find $vaults_dir -type d -name plugins -maxdepth 3) | sort | grep -v '^[./]' | uniq > "$outputs_dir/obsidian_plugins.txt"
+ls $(find $vaults_dir -type d -name plugins -maxdepth 3) | sort | egrep -v '^[./]' | uniq > "$outputs_dir/obsidian_plugins.txt"
 
 # note: maxdepth 3 is used to avoid listing plugins from nested obsidian vaults
